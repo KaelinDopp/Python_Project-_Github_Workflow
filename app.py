193af3e50ff9ae78d5ee4bdfe3603f71daae2509
KaelinDopp <kaelindopp@gmail.com>
2025-11-23 04:31:45 -0500

    Add formatted output displaying calculation results for predicted yearly savings.

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ savings = input("How many dollars did you save this month? ")
 
 savings = float(savings)
 yearly_savings = savings * 12
+
+print(f"You are predicted to save {yearly_savings} dollars this year.")
