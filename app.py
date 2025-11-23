43e74928b9273a294449064d3545d8880072f73b
KaelinDopp <kaelindopp@gmail.com>
2025-11-23 04:33:04 -0500

    Add error handling for user input based on whether they inputted a number or not.

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ print("Welcome to my Python program!")
 
 savings = input("How many dollars did you save this month? ")
 
-savings = float(savings)
+try:
+  savings = float(savings)
+except ValueError:
+  print("Please enter a valid number.")
+  exit()
+
 yearly_savings = savings * 12
 
 print(f"You are predicted to save {yearly_savings} dollars this year.")
